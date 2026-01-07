e3ece2a4bfaa55d1d1b1c125a6a260778e81b5ef
RaviAnand Mohabir <moravrav@gmail.com>
2026-01-07 12:50:43 +0100

    fix: :bug: set basename in router

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import '@/styles/globals.css';
 
 import App from './App.tsx';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from './provider.tsx';
-import '@/styles/globals.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Provider>
         <App />
       </Provider>
