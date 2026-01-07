0512d9daa6ec3f0d9ba6d60d03143fe7c9ca5985
RaviAnand Mohabir <moravrav@gmail.com>
2026-01-07 12:59:18 +0100

    feat: :sparkles: add umami tracking script to landing page

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,16 @@ import IndexPage from '@/pages/index';
 
 function App() {
   return (
-    <Routes>
-      <Route element={<IndexPage />} path="/" />
-      <Route element={<DocsPage />} path="/docs" />
-    </Routes>
+    <>
+      {process.env.NODE_ENV !== 'development' && (
+        <script defer src="https://cloud.umami.is/script.js" data-website-id="a8815709-2029-4757-8ed5-73d504673629" />
+      )}
+
+      <Routes>
+        <Route element={<IndexPage />} path="/" />
+        <Route element={<DocsPage />} path="/docs" />
+      </Routes>
+    </>
   );
 }
 
